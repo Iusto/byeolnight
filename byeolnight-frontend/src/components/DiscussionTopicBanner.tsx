2965bb3b108d4dfe6dc146a452033535c077aa65
iusto <iusto@naver.com>
2025-07-21 15:50:14 +0900

    markdwonts 통합

--- byeolnight-frontend/src/components/DiscussionTopicBanner.tsx
+++ byeolnight-frontend/src/components/DiscussionTopicBanner.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from '../lib/axios';
-import { parseMarkdown } from '../utils/markdown';
+import { parseMarkdown } from '../utils/markdownParser';
 
 interface DiscussionTopic {
   id: number;
