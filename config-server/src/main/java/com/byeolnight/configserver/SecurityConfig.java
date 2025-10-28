a5f20ab6d7601d22038a116b2ef42e437c5772b5
iusto <iusto@naver.com>
2025-10-29 03:38:51 +0900

    fix: config-server 환경변수 의존성 제거 및 중앙화 설정 완성

--- config-server/src/main/java/com/byeolnight/configserver/SecurityConfig.java
+++ config-server/src/main/java/com/byeolnight/configserver/SecurityConfig.java
@@ -1,13 +1,9 @@
 package com.byeolnight.configserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -16,22 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Value("${spring.security.user.name}")
-    private String username;
-
-    @Value("${spring.security.user.password}")
-    private String password;
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-            User.withUsername(username)
-                .password("{noop}" + password)
-                .roles("USER")
-                .build()
-        );
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
